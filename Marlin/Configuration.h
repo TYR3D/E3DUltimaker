5a49753883be8d713d47a30df5b160bd36d2e4eb
TYR3D <mmcgowan@tyr3d.com>
2016-11-12 12:27:06 +0000

    Tyr3d e3d hybrid changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -363,9 +363,9 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.0,80.0,200,837}  // default steps per unit for ultimaker2 [E3D]200
-#define DEFAULT_MAX_FEEDRATE          {300, 300, 40, 20}    // (mm/sec) [E3D]
-#define DEFAULT_MAX_ACCELERATION      {9000,9000,100,3000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot. [E3D]
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.0,80.0,200,282}  // default steps per unit for ultimaker2 [E3D]200
+#define DEFAULT_MAX_FEEDRATE          {300, 300, 40, 45}    // (mm/sec) [E3D]
+#define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot. [E3D]
 
 #define DEFAULT_ACCELERATION          3000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for retracts
